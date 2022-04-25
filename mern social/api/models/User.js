3020c69077f9149610c98925f4ae445c0f73bd34
ZhongLBuL <498467433@qq.com>
2022-04-24 20:53:30 -0400

    chatapp

---
+++ mern social/api/models/User.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose")
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        require: true,
+        min: 3,
+        max: 20,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        max: 50,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6
+    },
+    profilePicture: {
+        type: String,
+        default: ""
+    },
+    coverPicture: {
+        type: String,
+        default: ""
+    },
+    followers: {
+        type: Array,
+        default: []
+    },
+    followings: {
+        type: Array,
+        default: []
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    desc: {
+        type: String,
+        max: 50,
+        default: "I am a new user"
+    },
+    city: {
+        type: String,
+        max: 50,
+        default: "New York",
+    },
+    from: {
+        type: String,
+        max: 50,
+        default: "Boston",
+    },
+    relationship: {
+        type: Number,
+        enum: [1, 2, 3],
+        default: 1,
+    },
+},
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
